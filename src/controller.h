d6c5ffcd41ff78cca61997eac8811a66c47480f0
Tomas Delclaux <tomas@Tomass-iMac.local>
2020-05-31 14:32:36 +0200

    main project structure

---
+++ src/controller.h
@@ -0,0 +1,6 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+// Class definition
+
+#endif
---
+++ src/renderer.h
@@ -0,0 +1,10 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <vector>
+#include "SDL.h"
+
+
+// Class definition
+
+#endif
